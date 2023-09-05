22b96af958da936d950a74db1cad4f947a9a2418
Gabriel Anthony <ga13121110@gmail.com>
2023-09-04 23:54:45 -0300

    First commit

---
+++ TRABALHO_1/Models/Request/AlunoViewModel.cs
@@ -0,0 +1,21 @@
+﻿using TRABALHO_1.Models.Validations;
+
+namespace Aluno.Models.Request
+
+{
+    public class AlunoViewModel
+    {
+
+        public string Nome { get; set; }
+
+        [RaValidation]
+        public string RA { get; set; }
+
+        public string Email { get; set; }
+
+        public string Cpf { get; set; }
+
+        public bool Ativo { get; set; }
+
+    }
+}
